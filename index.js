b18298540325d6cd27a4d04b28ea1f7fbc10ed58
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2017-02-13 17:03:06 -0500

    can rotate Line into down position from right

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ function createTable() {
 }
 
 //inserShapeInGameTable returns the shape coordinates:
+//NEED TO REFACTOR AS SHAPE IS CHANGED!!!!!!!!!!!!!!!!!!!!!!
 function insertShapeInGameTable(gameTable, shape) {
 	//Remove the first 4 arrays in the gameTable with the shape:
 	var coords = {};
@@ -110,40 +111,64 @@ Square.prototype.rotate = function() {
 function Line() {
 	Shape.call(this, 'line', 7);
 	//Line beginning coordinates
-	this.coords = [[1, this.index],[2, this.index], [3, this.index], [4, this.index]];
+	this.coords = [[0, this.index],[1, this.index], [2, this.index], [3, this.index]];
 	//The Line shape default position: right
 	this.position = 'right';
 }
 
-//Rotate function for Line Shape:
-	//The positions can be: up, right, down, left;
-	//example of right position and pivotal point(*):
-	// |    |    |  X  |    |
-    //
-	// |    |    |  X  |    |
-	//           *
-	// |    |    |  X  |    |
-    //
-	// |    |    |  X  |    |
+
 
 Line.prototype.rotate = function() {
 	swith(this.position) {
+		//Rotate function for Line Shape:
+		//The positions can be: up, right, down, left;
+		//this is the default RIGHT position and pivotal point(*):
+		// |    |    |  X  |    |
+	    //
+		// |    |    |  X  |    |
+		//           *
+		// |    |    |  X  |    |
+	    //
+		// |    |    |  X  |    |
 		case 'right':
-			//rotating the line in 'down' position the element that remain is position
-			//has the following coordinates:
-			var x = this.coords[3][0];
-			var y = this.coords[3][1];
+			var x = this.coords[2][0];
+			var y = this.coords[2][1];
 			// it should be possible to rotate even if the line is on the far left or far right
 			//so in that case the pivotal point will change by 2 or 1 steps depending:
 			if(y < 2) y = 2;
 			if(y > 7) y = 7;
 			var newYCoords = [y-2, y-1, y, y+1];
 			this.coords = this.coords.map(function(coord, i) {
-				return [x, newCoords[i]];
+				return [x, newYCoords[i]];
 			})
 			this.position = 'down';
+		    //rotating the line in DOWN position from right:
+			// |     |     |     |     |
+		    //
+			// |     |     |     |     |
+			//             *
+			// |  X  |  X  |  X  |  X  |
+		    //
+			// |     |     |     |     |
+			//has the following coordinates: 
 			break;
 		case 'down':
+			var x = this.coords[1][0];
+			var y = this.coords[1][1];
+			if(x < )
+			var newXCoords = [x-2, x-1, x, x+1];
+			this.coords = this.coords.map(function(coord,i) {
+				return [newXCoords[i], y]
+			})
+			this.position = 'left';
+			//From down the Line rotate to a LEFT position:
+			// |    |  X  |    |    |
+		    //
+			// |    |  X  |    |    |
+			//           *
+			// |    |  X  |    |    |
+		    //
+			// |    |  X  |    |    |
 			break;
 		case 'left':
 			break;
